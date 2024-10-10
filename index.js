4e589b8403a5d3ff8adc7d63ea52b90b775a7fd2
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-10-10 14:55:08 +0530

    feat: added graceful server shutdown logic

--- index.js
+++ index.js
@@ -4,4 +4,24 @@ import logger from './utils/logger.js';
 const PORT = process.env.PORT;
 
 // Start the server
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+
+// Gracefully close the server on SIGINT (Ctrl+C)
+process.on('SIGINT', () => {
+  logger.info(`SIGINT signal received, closing the server...`);
+
+  server.close(() => {
+    console.log(`Closed the HTTP server gracefully.`);
+    process.exit(1);
+  });
+});
+
+// Gracefully close the server on SIGINT (Ctrl+z)
+process.on('SIGTERM', () => {
+  logger.info(`SIGINT signal received, closing the server...`);
+
+  server.close(() => {
+    console.log(`Closed the HTTP server gracefully.`);
+    process.exit(1);
+  });
+});
